5d792e0500febaf7e89f6537e3f4cbfbdc3af6fc
unknown <vankudregoutam15@gmail.com>
2024-05-13 13:38:07 +0530

    first commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'YOUR_API_KEY'
+const mongoURI = 'mongodb+srv://vankudregoutam15:8yMnYODoawSfVPZ6@cluster0.8aveumu.mongodb.net/eVoting?retryWrites=true&w=majority&appName=Cluster0'
 
 const  connectToMongo = () => {
     mongoose.connect(mongoURI)
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const connectToMongo = require('./db')
 const express = require('express')
 const cors = require('cors');
-// const path = require('path')
+const path = require('path')
 
 connectToMongo()
 
 const app = express()
 const port = 5000
+
+const _dirname=path.dirname("")
+const buildpath = path.join(_dirname,"../client/build")
+app.use(express.static(buildpath))
 
 // Enable CORS with the options
 app.use(cors());
