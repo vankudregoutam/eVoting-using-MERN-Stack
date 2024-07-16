3884c31e17d251f29b69d586e37e3286ef4884ce
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-07-16 16:06:56 +0530

    Update CandidateItem.js

--- client/src/components/CandidateItem.js
+++ client/src/components/CandidateItem.js
@@ -9,10 +9,11 @@ const CandidateItem = (props) => {
         <div className='col-md-3'>
             <div className="card my-3">
                 <div className="card-body">
+                    <img className="card-img-top" style={{ borderBottomLeftRadius: '5px', borderBottomRightRadius: '5px', marginBottom: '8px' }} src={candidate.url} alt={candidate.name} />
                     <h5 className="card-title">{candidate.partyname}</h5>
                     <p className="card-text">{candidate.name}</p>
-                    <i className="fa-regular fa-trash-can mx-2" style={{color: 'red'}} onClick={() => {deleteCandidate(candidate._id)}}></i>
-                    <i className="fa-regular fa-pen-to-square mx-2" style={{color: 'green'}} onClick={() => {updateCandidate(candidate)}}></i>
+                    <i className="fa-regular fa-trash-can mx-2" style={{ color: 'red' }} onClick={() => { deleteCandidate(candidate._id) }}></i>
+                    <i className="fa-regular fa-pen-to-square mx-2" style={{ color: 'green' }} onClick={() => { updateCandidate(candidate) }}></i>
                 </div>
             </div>
         </div>
