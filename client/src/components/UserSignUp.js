8959fd52475ca83bdfbbb7a7b14fee38a3f05995
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-07-16 11:37:56 +0530

    first commit

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -11,7 +11,7 @@ const UserSignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         const { name, id, password, cpassword } = credentials
-        const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+        const response = await fetch(`${process.env.BACKEND_URI}/createuser`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
