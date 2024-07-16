a0ff0709bfb6468b38b1cc571ecda65123af0d0f
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-07-16 11:36:46 +0530

    first commit

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -9,7 +9,7 @@ const UserLogin = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch(`${process.env.BACKEND_URI}/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
