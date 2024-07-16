0ea5a3b9a6c9b2d59a6cee540724599c099ee077
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-07-16 11:38:42 +0530

    first commit

--- client/src/components/AdminLogin.js
+++ client/src/components/AdminLogin.js
@@ -9,7 +9,7 @@ const AdminLogin = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch(`http://localhost:5000/api/auth/admin-login`, {
+        const response = await fetch(`${process.env.BACKEND_URI}/admin-login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
